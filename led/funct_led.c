33116b25ac8804783722cac706c129661cc623ce
Simone Ruffini <50401154+simoneruffini@users.noreply.github.com>
2023-02-22 16:27:25 +0100

    feat: added LED_Out_Range

--- led/funct_led.c
+++ led/funct_led.c
@@ -11,7 +11,7 @@
 #include "lpc17xx.h"
 #include "led.h"
 
-#define LED_NUM     8
+
 const unsigned long led_mask[] = { 1UL<<0, 1UL<<1, 1UL<<2, 1UL<< 3, 1UL<< 4, 1UL<< 5, 1UL<< 6, 1UL<< 7 };
 
 extern unsigned char led_value;
@@ -49,6 +49,21 @@ void LED_Out(unsigned int value) {
   }
 	led_value = LPC_GPIO2->FIOPIN;
 	
+}
+
+void LED_Out_Range(unsigned int value, uint8_t from_led_num, uint8_t to_led_num){
+	int i;
+	int j;
+	if (to_led_num <= from_led_num || to_led_num > LED_NUM) return;
+	
+  for (i = from_led_num, j=0; i <= to_led_num; i++,j++) {
+    if (value & (1<<j)) {
+      LED_On (i);
+    } else {
+      LED_Off(i);
+    }
+  }
+	led_value = LPC_GPIO2->FIOPIN;
 }
 /******************************************************************************
 **                            End Of File
--- led/led.h
+++ led/led.h
@@ -7,9 +7,15 @@
 ** Correlated files:    lib_led.c, funct_led.c
 **--------------------------------------------------------------------------------------------------------       
 *********************************************************************************************************/
+#include <stdio.h>
 
 #ifndef __LED_H
 #define __LED_H
+
+// total number of available leds, don't change
+// 0 is the rightmost led called led11 on the board
+// 8 is the leftmost led called led4 on the board
+#define LED_NUM 8
 
 /* lib_led */
 void LED_init(void);
@@ -19,6 +25,15 @@ void LED_deinit(void);
 void LED_On (unsigned int num);
 void LED_Off (unsigned int num);
 void LED_Out(unsigned int value);
+
+/**
+*@brief display a decimal value with binary encoding on a range of leds
+*@param value the value to display on the range of leds
+*@param from_led_num led position for range start
+*@param to_led_num led position for range end
+*@note LED_NUM >= to_led_num > from_led_num >= 0
+*/
+void LED_Out_Range(unsigned int value, uint8_t from_led_num, uint8_t to_led_num);
 
 /******************************************************************************
 **                            End Of File
